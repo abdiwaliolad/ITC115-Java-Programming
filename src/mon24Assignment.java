d615485e236ea30e366f567063e8fcef2efc577a
abdiwaliolad <abdiwaliolad@gmail.com>
2018-10-22 16:22:22 -0700

    method-with-two-parameters

---
+++ src/mon24Assignment.java
@@ -0,0 +1,30 @@
+/*
+ * Projact name: Season with two parameter method
+ * Date: 10.22.2018
+ * Author: Abdiwali M Olad
+ */
+
+
+
+public class mon24Assignment {
+
+	public static void main(String[] args) {
+
+		int result; // defin the variable name as int result
+
+		result = season(10, 5); // result now is called function method pass by
+								// two parameters
+
+		System.out.print(result); // called system out to print out the result
+									// in to the console.
+
+	}
+
+	public static int season(int x, int y) { // creating method and defin to
+												// parameters integers
+
+		return x + y; // return x and y
+
+	}// end of the method
+
+}//end class
